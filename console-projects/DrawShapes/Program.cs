e1fda89aa81ab093147337cf190d2a513b0a7cf2
louJ-dev <jericfactor@gmail.com>
2024-10-22 16:23:09 +0800

    add graphic selection add change base size with left and right arrow keys add live shape view add live status bar on top add menu split: shape list and live shape view add TextLimiter function q

--- console-projects/DrawShapes/Program.cs
+++ console-projects/DrawShapes/Program.cs
@@ -2,15 +2,8 @@
 
 public class Program{
     static void Main(){
-        int baseSize = -1;
-        do{
-            Console.Clear();
-            Console.WriteLine("[?] number >= +3 and isOdd");
-            Console.Write("Enter a number: ");
-            baseSize = int.Parse(Console.ReadLine() ?? "0");
-        } while(!(baseSize >= 3 && baseSize % 2 != 0));
+        const int menu_width = 38;
 
-        int index = 0;
         string[] nameShapes = new string[]{
             "line", "line-striped", "square",
             "parallelogram", "triangle", "triangle-reverse",
@@ -18,123 +11,227 @@ public class Program{
             "diamond", "zero", "arrow-up",
             "arrow-down", "X", "bow-tie"
         };
+
+        // usable graphics for shapes...
+        char[,] graphics = new char[,]{
+            {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'},
+            {'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D'},
+            {'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'},
+            {'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},
+            {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{'},
+            {'}', '[', ']', '|', '\\', ':', ';', '\"', '\'', '<', '>', ',', '.', '?', '/'},
+            {'~', '`', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+        };      
+
+        Console.Title = "Draw Shapes: ";
+
+        int baseSize = 7;
+        (int x, int y) indexGraphicA = (7, 4);
+        (int x, int y) indexGraphicB = (10, 4);
+        
+        // Create Menu
+        int index = 0; 
         while(true){
             Console.Clear();
-            Console.WriteLine($"Draw {nameShapes[index]} w/ base size {baseSize}\n");
+            Console.ResetColor();
+            Console.WriteLine($"Draw [{nameShapes[index]}] w/ size [{baseSize}]");
+            Console.WriteLine(TextLimiter(string.Empty, menu_width, '-') + TextLimiter("+", menu_width, '-'));
+            Console.ForegroundColor = ConsoleColor.Cyan;
             for(int i = 0; i < nameShapes.Length; i++){
+                string line = string.Empty;
+                
+                int lineWidth = menu_width;
                 if(i == index){
-                    Console.WriteLine($"> {nameShapes[i]}");
-                }else{
-                    Console.WriteLine($"{nameShapes[i]}");
+                    line += "> ";
+                    lineWidth -= 2;
                 }
+
+                line += TextLimiter(nameShapes[i], lineWidth);
+                Console.WriteLine(line + "|");
             }
-            Console.WriteLine("\nUP ARROW\t- move pointer up");
-            Console.WriteLine("DOWN ARROW\t- move pointer down");
-            Console.WriteLine("ENTER\t\t- select shape");
-            
+           
+            // show instructions...
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(TextLimiter(string.Empty, menu_width, '_') + "|");
+            Console.WriteLine(TextLimiter("UP ARROW     - move pointer up", menu_width) + "|");
+            Console.WriteLine(TextLimiter("DOWN ARROW   - move pointer down", menu_width) + "|");
+            Console.WriteLine(TextLimiter("LEFT ARROW   - decrease shape size", menu_width) + "|");
+            Console.WriteLine(TextLimiter("RIGHT ARROW  - increase shape size", menu_width) + "|");
+            Console.WriteLine(TextLimiter("WASD         - move graphicA selector", menu_width) + "|");
+            Console.WriteLine(TextLimiter("IJKL         - move graphicB selector", menu_width) + "|");
+            Console.WriteLine(TextLimiter("ENTER        - select shape", menu_width) + "|");
+            Console.WriteLine(TextLimiter(string.Empty, menu_width, '-') + "+");
+
+            for(int y = 0; y < graphics.GetLength(0); y++){
+                for (int x = 0; x < graphics.GetLength(1); x++){
+                    if(x == indexGraphicA.x && y == indexGraphicA.y || x == indexGraphicB.x && y == indexGraphicB.y){
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write($"[{graphics[y, x]}] ");
+                    }else{
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(graphics[y, x] + " ");
+                    }
+                }
+                Console.Write('\n');
+            }
+                 
+            Shapes.graphicA = graphics[indexGraphicA.y, indexGraphicA.x];
+            Shapes.graphicB = graphics[indexGraphicB.y, indexGraphicB.x];
+
+            // show live shape view...
+            DrawSelectedShape(index, baseSize, menu_width + 2, 2);
+
+            // Process Inputs
             ConsoleKeyInfo input = Console.ReadKey();
-            
-            // Move index
+
+            // move menu pointer..
             if(input.Key == ConsoleKey.UpArrow){
                 index--;
+
+                // set list boundaries
+                if(index < 0){
+                    index = 0;
+                }
             }else if(input.Key == ConsoleKey.DownArrow){
                 index++;
-            }
-            
-            // Set list boundaries
-            if(index >= nameShapes.Length){
-                index = nameShapes.Length - 1;
-            }else if(index < 0){
-                index = 0;
+
+                // set list boundaries
+                if(index >= nameShapes.Length){
+                    index = nameShapes.Length - 1;
+                }
             }
 
+            // increase or decrease shape size...
+            if(input.Key == ConsoleKey.RightArrow){
+                baseSize++;
+
+                if(baseSize > 49){
+                    baseSize = 49;
+                }
+            }else if(input.Key == ConsoleKey.LeftArrow){
+                baseSize--;
+            
+                 if(baseSize < 3){
+                    baseSize = 3;
+                }
+            }
+            
+            // move graphic_selector pointer A...
+            if(input.Key == ConsoleKey.W){
+                indexGraphicA.y--;
+                if(indexGraphicA.y < 0){
+                    indexGraphicA.y = 0;
+                }
+            }else if(input.Key == ConsoleKey.S){
+                indexGraphicA.y++;
+                if(indexGraphicA.y >= graphics.GetLength(0)){
+                    indexGraphicA.y = graphics.GetLength(0) - 1;
+                }
+            }else if (input.Key == ConsoleKey.D){
+                indexGraphicA.x++;
+                if(indexGraphicA.x >= graphics.GetLength(1)){
+                     indexGraphicA.x = graphics.GetLength(1) - 1;
+                }  
+            }else if (input.Key == ConsoleKey.A){
+                indexGraphicA.x--;
+                if(indexGraphicA.x < 0){
+                    indexGraphicA.x = 0;
+                }
+            }
+            
+            // move graphic_selector pointer B...
+            if(input.Key == ConsoleKey.I){
+                indexGraphicB.y--;
+                if(indexGraphicB.y < 0){
+                    indexGraphicB.y = 0;
+                }
+            }else if(input.Key == ConsoleKey.K){
+                indexGraphicB.y++;
+                if(indexGraphicB.y >= graphics.GetLength(0)){
+                    indexGraphicB.y = graphics.GetLength(0) - 1;
+                }
+            }else if (input.Key == ConsoleKey.L){
+                indexGraphicB.x++;
+                if(indexGraphicB.x >= graphics.GetLength(1)){
+                     indexGraphicB.x = graphics.GetLength(1) - 1;
+                }  
+            }else if (input.Key == ConsoleKey.J){
+                indexGraphicB.x--;
+                if(indexGraphicB.x < 0){
+                    indexGraphicB.x = 0;
+                }
+            }
+            
             // Draw selected
             if(input.Key == ConsoleKey.Enter){
                 break;
             }
         }
+        
+        // move cursor to bottom of window...
+        Console.SetCursorPosition(0, Console.BufferHeight - 2);
+        Console.WriteLine("END");  
+    }
 
+    static void DrawSelectedShape(int index, int baseSize, int posX, int posY){
         switch(index){
             case 0:
-                Shapes.DrawLine(baseSize);
+                Shapes.DrawLine(baseSize, posX, posY);
                 break;
             case 1:
-                Shapes.DrawStripedLine(baseSize);
+                Shapes.DrawStripedLine(baseSize, posX, posY);
                 break;
             case 2:
-                Shapes.DrawSquare(baseSize);
+                Shapes.DrawSquare(baseSize, posX, posY);
                 break;
             case 3:
-                Shapes.DrawParallelogram(baseSize);
+                Shapes.DrawParallelogram(baseSize, posX, posY);
                 break;
             case 4:
-                Shapes.DrawTriangle(baseSize);
+                Shapes.DrawTriangle(baseSize, posX, posY);
                 break;
             case 5:
-                Shapes.DrawTriangleRev(baseSize);
+                Shapes.DrawTriangleRev(baseSize, posX, posY);
                 break;
             case 6:
-                Shapes.DrawIsocelesTriangle(baseSize);
+                Shapes.DrawIsocelesTriangle(baseSize, posX, posY);
                 break;
             case 7:
-                Shapes.DrawIsocelesTriangleRev(baseSize);
+                Shapes.DrawIsocelesTriangleRev(baseSize, posX, posY);
                 break;
             case 8:
-                Shapes.DrawHourGlass(baseSize);
+                Shapes.DrawHourGlass(baseSize, posX, posY);
                 break;
             case 9:
-                Shapes.DrawDiamond(baseSize);
+                Shapes.DrawDiamond(baseSize, posX, posY);
                 break;
             case 10:
-                Shapes.DrawZero(baseSize);
+                Shapes.DrawZero(baseSize, posX, posY);
                 break;
             case 11:
-                Shapes.DrawArrowUp(baseSize);
+                Shapes.DrawArrowUp(baseSize, posX, posY);
                 break;
             case 12:
-                Shapes.DrawArrowDown(baseSize);
+                Shapes.DrawArrowDown(baseSize, posX, posY);
                 break;
             case 13:
-                Shapes.DrawX(baseSize);
+                Shapes.DrawX(baseSize, posX, posY);
                 break;
             case 14:
-                Shapes.DrawBowTie(baseSize);
+                Shapes.DrawBowTie(baseSize, posX, posY);
                 break;
         }
+    }
 
-        Console.WriteLine("END");
-        
-        /* TEST
-        Console.WriteLine("\nline...");
-        Shapes.DrawLine(testVal);
-        Console.WriteLine("\nstriped...");
-        Shapes.DrawStripedLine(testVal);
-        Console.WriteLine("\nsquare...");
-        Shapes.DrawSquare(testVal);
-        Console.WriteLine("\nparallelogram...");
-        Shapes.DrawParallelogram(testVal);
-        Console.WriteLine("\ntriangle...");
-        Shapes.DrawTriangle(testVal);
-        Console.WriteLine("\ntriangle.reverse...");
-        Shapes.DrawTriangleRev(testVal);
-        Console.WriteLine("\nisoceles.triangle...");
-        Shapes.DrawIsocelesTriangle(testVal);
-        Console.WriteLine("\nisoceles.traingle.reverse...");
-        Shapes.DrawIsocelesTriangleRev(testVal);
-        Console.WriteLine("\nhourglass...");
-        Shapes.DrawHourGlass(testVal);
-        Console.WriteLine("\ndiamond...");
-        Shapes.DrawDiamond(testVal);
-        Console.WriteLine("\nzero...");
-        Shapes.DrawZero(testVal);
-        Console.WriteLine("\narrow.up...");
-        Shapes.DrawArrowUp(testVal);
-        Console.WriteLine("\narrow.down...");
-        Shapes.DrawArrowDown(testVal);
-        Console.WriteLine("\nx...");
-        Shapes.DrawX(testVal);
-        Console.WriteLine("\nbow.tie...");
-        Shapes.DrawBowTie(testVal);
-        */
+    static string TextLimiter(string text, int lineWidth, char empty=' '){
+        string result = string.Empty;
+        for (int i = 0; i < lineWidth; i++){
+            if(i < text.Length){
+                result += text[i];
+            }else{
+                result += empty;
+            }
+        }
+        return result;
     }
 }
--- console-projects/DrawShapes/src/Shapes.cs
+++ console-projects/DrawShapes/src/Shapes.cs
@@ -1,136 +1,182 @@
 ﻿namespace DrawShapes;
 
 public static class Shapes{
-	public static void DrawLine(int b){
+
+    public static char graphicA;
+    public static char graphicB;
+
+    static Shapes(){
+        graphicA = '*';
+        graphicB = '_';
+    }
+
+	public static void DrawLine(int b, int posX = 0, int posY = 0){
 	    string line = string.Empty;
 	    for (int i = 0; i < b; i++){
-	        line += "*";
+	        line += graphicA;
 	    }
-	    Console.WriteLine(line);
+        
+        // Moving the shape in x axis 
+        Console.SetCursorPosition(posX, posY++);
+	    
+        Console.WriteLine(line);
 	}
 
-	public static void DrawStripedLine(int b){
+	public static void DrawStripedLine(int b, int posX = 0, int posY = 0){
 	    string line = string.Empty;
 	    for(int i = 1; i <= b; i++){
 	        if((i % 2) == 0){
-	            line += "_";
+	            line += graphicB;
 	        }else{
-	            line += "*";
+	            line += graphicA;
 	        }
 	    }
-	    Console.WriteLine(line);
+        
+        // Moving the shape in x axis 
+        Console.SetCursorPosition(posX, posY++);
+	    
+        Console.WriteLine(line);
 	}
 
-	public static void DrawSquare(int b){
-	    for(int y = 1; y <= b; y++){
+	public static void DrawSquare(int b, int posX = 0, int posY = 0){
+	    // int drawPosX = Console.GetCursorPosition().Left + 1;
+        for(int y = 1; y <= b; y++){
 	        string line = string.Empty;
-	        for (int x = 0; x < b; x++){
-	            line += "*"; 
+            for (int x = 0; x < b; x++){
+	            line += graphicA + " "; 
 	        }
-	        Console.WriteLine(line);
-	    }
+
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+            
+            Console.WriteLine(line);
+	    } 
 	}
 
-	public static void DrawParallelogram(int b){
+	public static void DrawParallelogram(int b, int posX = 0, int posY = 0){
 	    int left = b;
 	    int right = b * 2;
 	    for(int y = 1; y <= b; y++){
 	        string line = string.Empty;
 	        for(int x = 1; x <= b * 2; x++){
 	            if(x >= left && x <= right){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
 	        }
-	        Console.WriteLine(line);
+	    
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	        right--;
 	        left--;
 	    }
 	}
 
-	public static void DrawTriangle(int b){
+	public static void DrawTriangle(int b, int posX = 0, int posY = 0){
 	    int pointer = b;
 	    for(int y = 1; y <= b; y++){
 	        string line = string.Empty;
 	        for(int x = 1; x <= b; x++){
 	            if(x <= pointer){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
 	        }
-	        Console.WriteLine(line);
+	   
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	        pointer--;
 	    }
 	}
 
-	public static void DrawTriangleRev(int b){
+	public static void DrawTriangleRev(int b, int posX = 0, int posY = 0){
 	    int pointer = 1;
 	    for(int y = 1; y <= b; y++){
 	        string line = string.Empty;
 	        for(int x = 1; x <= b; x++){
 	            if(x <= pointer){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
-	        }
-	        Console.WriteLine(line);
+	        }  
+
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+            
+            Console.WriteLine(line);
 	        pointer++;
 	    }
 	}
 
-	public static void DrawIsocelesTriangle(int b){
+	public static void DrawIsocelesTriangle(int b, int posX = 0, int posY = 0){
 	    int left = (b - 1) / 2 + 1; 
 	    int right = (b - 1) / 2 + 1;
 	    for(int y = 1; y <= (b - 1) / 2 + 1; y++){
 	        string line = string.Empty;
 	        for (int x = 1; x <= b; x++){
 	            if(x >= left && x <= right){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
 	        }
-	        Console.WriteLine(line);
+
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	        left--;
 	        right++;
 	    }
 	}
 
 
-	public static void DrawIsocelesTriangleRev(int b) {
+	public static void DrawIsocelesTriangleRev(int b, int posX = 0, int posY = 0) {
 	    int left = 1;
 	    int right = b;
 	    for (int y = 1; y <= (b - 1) / 2 + 1; y++){
 	        string line = string.Empty;
 	        for (int x = 1; x <= b; x++){
 	            if(x >= left && x <= right){
-	                line += "*"; 
+	                line += graphicA; 
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
 	        }
-	        Console.WriteLine(line);
+
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	        left++;
 	        right--;
 	    }
 	}
 
-	public static void DrawHourGlass(int b) {
+	public static void DrawHourGlass(int b, int posX = 0, int posY = 0) {
 	    int left = 1;
 	    int right = b;
 	    for(int y = 1; y <= b; y++){
 	        string line = string.Empty;
 	        for (int x = 1; x <= b; x++){
 	            if(x >= left && x <= right){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
 	        }
-	        Console.WriteLine(line);
+
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	        
 	        if(y < ((b - 1) / 2 + 1)){
 	            left++;
@@ -142,19 +188,23 @@ public static class Shapes{
 	    }
 	}
 
-	public static void DrawDiamond(int b) {
+	public static void DrawDiamond(int b, int posX = 0, int posY = 0) {
 	    int left = (b - 1) / 2 + 1;
 	    int right = left;
 	    for(int y = 1; y <= b; y++){
 	        string line = string.Empty;
 	        for(int x = 1; x <= b; x++){
 	            if(x >= left && x <= right){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
 	        }
-	        Console.WriteLine(line);
+
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	        if(y < (b - 1) / 2 + 1){
 	            left--;
 	            right++;
@@ -165,40 +215,62 @@ public static class Shapes{
 	    }
 	}    
 
-	public static void DrawZero(int b) {
+	public static void DrawZero(int b, int posX = 0, int posY = 0) {
 	    
 	    string line = string.Empty;
 	    for(int x = 0; x < b; x++){
-	        line += "*";
+	        line += graphicA;
 	    }
-	    Console.WriteLine(line);
+	    
+        // Moving the shape in x axis 
+        Console.SetCursorPosition(posX, posY++);
+
+        Console.WriteLine(line);
 	    for(int y = 2; y < b; y++){
-	        line = "*";
+	        line = string.Empty + graphicA;
 	        for(int x = 2; x < b; x++){
-	            line += "_";
+	            line += graphicB;
 	        }
-	        line += "*";
-	        Console.WriteLine(line);
+	        line += graphicA;
+	        
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	    }
 	    
 	    line = string.Empty;
 	    for(int x = 0; x < b; x++){    
-	        line += "*";
+	        line += graphicA;
 	    }
-	    Console.WriteLine(line);
+
+        // Moving the shape in x axis 
+        Console.SetCursorPosition(posX, posY++);
+
+        Console.WriteLine(line);
 	}
 
-	public static void DrawArrowUp(int b) {
+	public static void DrawArrowUp(int b, int posX = 0, int posY = 0) {
 	    int left = (b - 1) / 2 + 1;
 	    int right = left;
 	    int half = (b - 1) / 2 + 1;
+
+        ConsoleColor[] colors = new ConsoleColor[5];
+        colors[0] = ConsoleColor.Green;
+        colors[1] = ConsoleColor.Cyan;
+        colors[2] = ConsoleColor.Yellow;
+        colors[3] = ConsoleColor.DarkMagenta;
+        colors[4] = ConsoleColor.DarkGray;
+
+        int c = 0;
+
 	    for(int y = 1; y <= b; y++){
 	        string line = string.Empty;
 	        for(int x = 1; x <= b; x++){
 	            if(x >= left && x <= right){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
 	        }
 	        if(y < half){
@@ -208,11 +280,19 @@ public static class Shapes{
 	            left = half - ((half - 1) / 2);
 	            right = half + ((half - 1) / 2);
 	        }
-	       Console.WriteLine(line);
+	   
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+            c++;
+            if(c == 5){
+                c = 0;
+            }
+            Console.ForegroundColor = colors[c];
+            Console.WriteLine(line);
 	    }
 	}
 
-	public static void DrawArrowDown(int b) {
+	public static void DrawArrowDown(int b, int posX = 0, int posY = 0) {
 	    int half = (b - 1) / 2 + 1;
 	    int left = half - (half - 1) / 2;
 	    int right = half + (half - 1) / 2;
@@ -220,12 +300,16 @@ public static class Shapes{
 	        string line = string.Empty;
 	        for (int x = 1; x <= b; x++){
 	            if(x >= left && x <= right){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_";
+	                line += graphicB;
 	            }
 	        }
-	        Console.WriteLine(line);
+	  
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	        if(y == half - 1){
 	            left = 1;
 	            right = b;
@@ -238,19 +322,23 @@ public static class Shapes{
 	    }
 	}
 
-	public static void DrawX(int b) {
+	public static void DrawX(int b, int posX = 0, int posY = 0) {
 	    int left = 1;
 	    int right = b;
 	    for (int y = 1; y <= b; y++){
 	        string line = string.Empty;
 	        for (int x = 1; x <= b; x++){
 	            if(x == left || x == right){
-	                line += "*";
+	                line += graphicA;
 	            }else{
-	                line += "_"; 
+	                line += graphicB; 
 	            }
 	        }
-	        Console.WriteLine(line);
+	 
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	        if(y >= (b - 1) / 2 + 1){
 	            left--;
 	            right++;
@@ -261,16 +349,16 @@ public static class Shapes{
 	    }
 	}
 
-	public static void DrawBowTie(int b) {
+	public static void DrawBowTie(int b, int posX = 0, int posY = 0) {
 	    int left = 1;
 	    int right = b;
 	    for (int y = 1; y <= b; y++){
 	        string line = string.Empty;
 	        for (int x = 1; x <= b; x++){
 	            if(x > left && x < right){
-	                line += "_";
+	                line += graphicB;
 	            }else{
-	                line += "*";
+	                line += graphicA;
 	            }
 	        }
 	        if(y > (b - 1) / 2){
@@ -280,7 +368,11 @@ public static class Shapes{
 	            left++;
 	            right--;
 	        }
-	        Console.WriteLine(line);
+	
+            // Moving the shape in x axis 
+            Console.SetCursorPosition(posX, posY++);
+
+            Console.WriteLine(line);
 	    }
 	}
 }
