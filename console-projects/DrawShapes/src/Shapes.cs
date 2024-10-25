fceba117dc54bd738d7950ef56e1d32de3445a21
unknown <jericfactor@gmail.com>
2024-10-26 02:01:47 +0800

    update from windows version test

--- console-projects/DrawShapes/src/Shapes.cs
+++ console-projects/DrawShapes/src/Shapes.cs
@@ -15,7 +15,7 @@ public static class Shapes{
         graphicA = '*';
         graphicB = '_';
         enableColorChange = false;
-        drawDelay = 100;
+        drawDelay = 50;
 
         indexColor = 0;
 
